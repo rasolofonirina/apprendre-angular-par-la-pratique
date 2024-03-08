10a3a9ff7d5d9438221f34a0672f6fbe26df7b4a
Eric Rasolofonirina <ericseven.mg@gmail.com>
2024-03-08 14:46:22 +0100

    Add 404 and default route

--- project/gdg-antsirabe/src/app/app.routes.ts
+++ project/gdg-antsirabe/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { EventListComponent } from './events/event-list/event-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'members', component: MemberListComponent },
   { path: 'events', component: EventListComponent },
+  { path: '', redirectTo: '/members', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
