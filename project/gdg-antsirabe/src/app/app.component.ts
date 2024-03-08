cfda8c7b8d5cb5d7b90fe8c6b6bdecdb5cfff1ca
Eric Rasolofonirina <ericseven.mg@gmail.com>
2024-03-08 12:59:48 +0100

    Replace favicon and add navigation component

--- project/gdg-antsirabe/src/app/app.component.ts
+++ project/gdg-antsirabe/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavigationComponent]
 })
 export class AppComponent {
   title = 'gdg-antsirabe';
